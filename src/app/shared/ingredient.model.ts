1811b689090588050f1d96645cd5d0e972caf292
Antonio Marrero Bonilla <gaming_marrero@yahoo.com>
2018-01-29 15:51:49 -0500

    Updated

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,14 @@
+export class Ingredient {
+    // public name: string;
+    // public amount: number;
+
+    // constructor(name: string, amount: number) {
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+
+    constructor(public name: string, public amount: number) {
+        // tslint:disable-next-line:no-trailing-whitespace
+        
+    }
+}
